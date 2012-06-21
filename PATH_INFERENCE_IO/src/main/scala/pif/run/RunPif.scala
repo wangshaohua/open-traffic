4aba1c768294fe6288d1133c72923183a39daa80
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2012-06-20 18:40:56 -0700

    generic script to run PIF

---
+++ PATH_INFERENCE_IO/src/main/scala/pif/run/RunPif.scala
@@ -0,0 +1,156 @@
+package pif.run
+
+
+package path_inference_mm
+import scopt.OptionParser
+import path_inference.PathInferenceParameters2
+import path_inference.crf.ComputingStrategy
+import path_inference.manager.ProjectionHook
+import java.io.File
+import core_extensions.MMLogging
+import path_inference.PathInferenceFilter
+import netconfig.Datum.ProbeCoordinate
+import netconfig.Link
+import path_inference.manager.ProjectionHookInterface
+import org.joda.time.LocalDate
+import netconfig.io.Dates
+import netconfig.io.Serializer
+import netconfig.io.files.RawProbe
+import netconfig.io.files.ProbeCoordinateViterbi
+import netconfig.io.files.PathInferenceViterbi
+import netconfig.io.files.TrajectoryViterbif
+import netconfig.storage.LinkIDRepr
+import network.gen.GenericLink
+import netconfig.io.json.JSonSerializer
+import netconfig.io.files.SerializedNetwork
+import network.gen.GenericLinkRepr
+import network.gen.NetworkBuilder
+
+/**
+ * Runs the path inference on some serialized data, using a generic network representation.
+ *
+ * It uses some default sensible parameters for best accuracy.
+ *
+ * mvn install
+ * 
+ * Debugging procedure for the PIF
+ * 
+ */
+object RunPif extends MMLogging {
+
+  def getLinks(network_id:Int, source_name:String):Seq[Link] = {
+    val fname = SerializedNetwork.fileName(network_id, source_name)
+    val glrs = JSonSerializer.getGenericLinks(fname)
+    val builder = new NetworkBuilder
+    builder.build(glrs)
+  }
+  
+  def getSerializer(network_id:Int, source_name:String, links:Seq[Link]): Serializer[Link] = {
+    val map: Map[LinkIDRepr, Link] = Map.empty ++ links.map(l => {
+      val linkId = l.asInstanceOf[GenericLink].idRepr
+      (linkId, l)
+    })
+    JSonSerializer.from(map)
+  }
+  
+  def main(args: Array[String]) = {
+    // All the options
+    import Dates._
+    var network_id: Int = -1
+    var date: LocalDate = null
+    var range: Seq[LocalDate] = Seq.empty
+    var feed: String = ""
+    var driver_id:String = ""
+    val parser = new OptionParser("test") {
+      intOpt("nid", "the net id", network_id = _)
+      opt("date", "the date", { s: String => { for (d <- parseDate(s)) { date = d } } })
+      opt("range", "the date", (s:String) => for(r <- parseRange(s)) {range = r})
+      arg("feed", "data feed", feed = _)
+      opt("driver_id", "Runs the filter on the selected driver id", driver_id = _)
+    }
+    parser.parse(args)
+
+    val parameters = pifParameters()
+    
+    var links: Seq[Link] = getLinks(network_id, feed)
+
+    val serialier: Serializer[Link] = getSerializer(network_id, feed, links)
+
+    val projection_hook: ProjectionHookInterface = ProjectionHook.create(links, parameters)
+
+    val date_range: Seq[LocalDate] = {
+      if (date != null) {
+        Seq(date)
+      } else {
+        range
+      }
+    }
+
+    val drivers_whitelist = Set(driver_id)
+    
+    assert(!date_range.isEmpty, "You must provide a date or a range of date")
+
+    println(date_range)
+    print("Feed:" + feed)
+
+    for (findex <- RawProbe.list(feed=feed, nid=network_id, dates=date_range)) {
+      runPIF(projection_hook, serialier, parameters, findex, drivers_whitelist)
+    }
+  }
+
+  def pifParameters() = {
+    val params = new PathInferenceParameters2()
+    params.fillDefaultFor1MinuteOffline
+    params.setReturnPoints(true)
+    params.setReturnRoutes(true)
+    params.setMinPathProbability(0.1)
+    params.setMinProjectionProbability(0.1)
+    params.setShuffleProbeCoordinateSpots(true)
+    params.setComputingStrategy(ComputingStrategy.LookAhead2)
+    params
+  }
+
+  def runPIF(projector: ProjectionHookInterface, serializer: Serializer[Link],
+    parameters: PathInferenceParameters2, file_index:RawProbe.FileIndex, drivers_whitelist:Set[String]): Unit = {
+    val fname_in = RawProbe.fileName(file_index)
+    val fname_pcs = ProbeCoordinateViterbi.fileName(feed = file_index.feed, 
+        nid = file_index.nid,
+        date = file_index.date,
+       net_type = "navteq")
+    val fname_pis = PathInferenceViterbi.fileName(feed = file_index.feed, 
+        nid = file_index.nid,
+        date = file_index.date,
+        net_type = "navteq")
+    val fname_trajs = (vid: String, traj_idx: Int) =>
+      TrajectoryViterbif.fileName(feed = file_index.feed, nid = file_index.nid, date = file_index.date,
+        net_type = "navteq", vid, traj_idx)
+
+    assert((new File(fname_in)).exists())
+
+    val writer_pi = serializer.writerPathInference(fname_pis)
+    val writer_pc = serializer.writerProbeCoordinate(fname_pcs)
+    val data = serializer.readProbeCoordinates(fname_in)
+    val pif = PathInferenceFilter.createManager(parameters, projector)
+    for (raw <- data) {
+      val pc = raw.asInstanceOf[ProbeCoordinate[Link]]
+      if (drivers_whitelist.isEmpty || pc.id==null || drivers_whitelist.contains(pc.id)) {
+        pif.addPoint(pc)
+        for (out_pi <- pif.getPathInferences) {
+          writer_pi.put(out_pi)
+        }
+        for (out_pc <- pif.getProbeCoordinates) {
+          writer_pc.put(out_pc)
+        }
+      }
+    }
+    pif.finalizeManager
+    for (out_pi <- pif.getPathInferences) {
+      writer_pi.put(out_pi)
+    }
+    for (out_pc <- pif.getProbeCoordinates) {
+      writer_pc.put(out_pc)
+    }
+    writer_pc.close()
+    writer_pi.close()
+  }
+}
---
+++ PATH_INFERENCE_IO/src/main/scala/pif/run/package.scala
@@ -0,0 +1,5 @@
+package pif
+
+package object run {
+
+}
