00520ccf2506e0dd8975856c63124a6ca55706f6
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2012-10-30 12:06:18 -0700

    style

--- ARTERIAL/src/test/scala/network/arterial1/ProjectionTest.scala
+++ ARTERIAL/src/test/scala/network/arterial1/ProjectionTest.scala
@@ -1,4 +1,21 @@
+/**
+ * Copyright 2012. The Regents of the University of California (Regents).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package network.arterial1
+
 import org.junit._
 import org.junit.Assert._
 import com.codahale.jerkson.Json._
@@ -22,7 +39,7 @@ class ProjectionTest extends MMLogging {
 
   @Test def test1 = {
     val lr = LinkIDRepr(93335, 0)
-    logInfo("link length: "+net(lr).length)
+    logInfo("link length: " + net(lr).length)
     val r1r = RouteRepr(Seq(lr), Seq(SpotRepr(lr, 154.151993), SpotRepr(lr, 154.151993)))
     val r2r = RouteRepr(Seq(lr), Seq(SpotRepr(lr, 154.151993), SpotRepr(lr, 154.151993)))
     val codec = JSonSerializer.from(net)
