31b34b806aa83b3f50e701513048caea041169ee
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2012-11-05 19:51:56 -0800

    DEBUG commit

--- NETCONFIG_IO/src/main/scala/netconfig/io/StringDataSink.scala
+++ NETCONFIG_IO/src/main/scala/netconfig/io/StringDataSink.scala
@@ -24,29 +24,29 @@ import java.io.BufferedOutputStream
 class StringDataSink(
   ostream: OutputStream,
   separator: String = "\n") extends DataSink[String] {
+
   def put(n: String) = {
     ostream.write(n.getBytes())
     ostream.write(separator.getBytes())
   }
 
   def close(): Unit = {
-    //    ostream.write(JsonConsts.end.getBytes())
     ostream.close()
   }
 }
 
 object StringDataSink extends MMLogging {
-  val buffer_size = 4000000 // Approx 4MB buffer
-//   logInfo("Buffer size: "+buffer_size)
+  val buffer_size = 40000 // Approx 4MB buffer
   def writeableZippedFile(fname: String): StringDataSink = {
     // Create the necessary directories.
+    // TODO(?) this only works on UNIX?
     val dir_name = fname.split("/").dropRight(1).mkString("/")
     val f = new File(dir_name)
     if (!f.exists()) {
       logInfo("Creating new directory: " + dir_name)
       f.mkdirs()
     }
-    val ostream = new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(fname), buffer_size))
+    val ostream = new GZIPOutputStream(new FileOutputStream(fname))
     new StringDataSink(ostream)
   }
 
